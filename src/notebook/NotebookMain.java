e6dd215601973d540fd228f3654409c9dce26e27
Haliulinvv <77212198+Haliulinvv@users.noreply.github.com>
2026-03-19 16:35:02 +0300

    Задача 3

--- src/notebook/NotebookMain.java
+++ src/notebook/NotebookMain.java
@@ -2,8 +2,6 @@ package notebook;
 
 public class NotebookMain {
 	public static void main(String[] args) {
-	        //ClientForm form = new ClientForm();
-	        //form.setVisible(true); // или вызвать метод, который показывает окно
-		new ClientForm().setVisible(true);
+		new ClientForm().setVisible(true); // вызвать метод, который показывает окно
 	}
 }
